a43c8fd764da00112cb5b033f25a6e79f2601274
KAC CHAN <kacxxchan@gmail.com>
2022-11-28 10:21:02 +0530

    Update admins.py

--- TOGA/Helpers/admins.py
+++ TOGA/Helpers/admins.py
@@ -4,16 +4,13 @@ from pyrogram.types import Message, CallbackQuery
 from pyrogram.enums import ChatMemberStatus
 
 from .active import is_active_chat
-from TOGA import app, 
+from TOGA import app
 
 
 def admin_check(func: Callable) -> Callable:
     async def non_admin(_, message: Message):
         if not await is_active_chat(message.chat.id):
             return await message.reply_text("ʙᴏᴛ ɪsɴ'ᴛ sᴛʀᴇᴀᴍɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.")
-
-        if message.from_user.id in SUDOERS:
-            return await func(_, message)
 
         check = await app.get_chat_member(message.chat.id, message.from_user.id)
         if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
@@ -32,9 +29,6 @@ def admin_check_cb(func: Callable) -> Callable:
     async def cb_non_admin(_, query: CallbackQuery):
         if not await is_active_chat(query.message.chat.id):
             return await query.answer("ʙᴏᴛ ɪsɴ'ᴛ sᴛʀᴇᴀᴍɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.", show_alert=True)
-
-        if query.from_user.id in SUDOERS:
-            return await func(_, query)
 
         check = await app.get_chat_member(query.message.chat.id, query.from_user.id)
         if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
