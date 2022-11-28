817933b9d2135d66a1cb70531454c82466224729
KAC CHAN <kacxxchan@gmail.com>
2022-11-28 09:40:32 +0530

    Update activevc.py

--- TOGA/Modules/activevc.py
+++ TOGA/Modules/activevc.py
@@ -1,15 +1,15 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from FallenMusic import app, SUDOERS
-from FallenMusic.Helpers.active import get_active_chats
-from FallenMusic.Helpers.inline import close_key
+from TOGA import app, SUDOERS
+from TOGA.Helpers.active import get_active_chats
+from TOGA.Helpers.inline import close_key
 
 
 @app.on_message(filters.command("activevc") & filters.user(SUDOERS))
 async def activevc(_, message: Message):
     mystic = await message.reply_text(
-        "» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ʟɪsᴛ..."
+        "» Getting Active VCs..."
     )
     chats = await get_active_chats()
     text = ""
@@ -18,7 +18,7 @@ async def activevc(_, message: Message):
         try:
             title = (await app.get_chat(chat)).title
         except Exception:
-            title = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+            title = "Private Chats"
         if (await app.get_chat(chat)).username:
             user = (await app.get_chat(chat)).username
             text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})\n"
@@ -26,10 +26,10 @@ async def activevc(_, message: Message):
             text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
         j += 1
     if not text:
-        await mystic.edit_text("ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄʙᴏᴛ...")
+        await mystic.edit_text("No Active VCs...")
     else:
         await mystic.edit_text(
-            f"**ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄ ʙᴏᴛ :**\n\n{text}",
+            f"**List Of Currently Active VCs..:**\n\n{text}",
             reply_markup=close_key,
             disable_web_page_preview=True,
         )
