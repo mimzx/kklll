577442a031c0d4984813829d9c9c37601fa75c40
KAC CHAN <kacxxchan@gmail.com>
2022-11-28 09:35:35 +0530

    Update __main__.py

--- TOGA/__main__.py
+++ TOGA/__main__.py
@@ -7,41 +7,41 @@ from pytgcalls import StreamType
 from pytgcalls.types.input_stream import AudioVideoPiped
 
 from .logging import LOGGER
-from FallenMusic import (app, app2, pytgcalls, SUNAME,
+from TOGA import (app, app2, pytgcalls, SUNAME,
                          BOT_ID, BOT_NAME, BOT_USERNAME,
                          ASS_ID, ASS_NAME, ASS_USERNAME)
-from FallenMusic.Modules import ALL_MODULES
+from TOGA.Modules import ALL_MODULES
 
 
 async def fallen_startup():
-    LOGGER("FallenMusic").info("[•] Loading Modules...")
+    LOGGER("TOGA").info("[•] Loading Modules...")
     for module in ALL_MODULES:
-        importlib.import_module("FallenMusic.Modules." + module)
-    LOGGER("FallenMusic").info(f"[•] Loaded {len(ALL_MODULES)} Modules.")
+        importlib.import_module("TOGA.Modules." + module)
+    LOGGER("TOGA").info(f"[•] Loaded {len(ALL_MODULES)} Modules.")
 
-    LOGGER("FallenMusic").info("[•] Refreshing Directories...")
+    LOGGER("TOGA").info("[•] Refreshing Directories...")
     if "downloads" not in os.listdir():
         os.mkdir("downloads")
     if "cache" not in os.listdir():
         os.mkdir("cache")
     if "raw_files" not in os.listdir():
         os.mkdir("raw_files")
-    LOGGER("FallenMusic").info("[•] Directories Refreshed.")
+    LOGGER("TOGA").info("[•] Directories Refreshed.")
 
     try:
-        await app.send_message(SUNAME, f"✯ ғᴀʟʟᴇɴ ᴍᴜsɪᴄ ʙᴏᴛ ✯\n\n𖢵 ɪᴅ : `{BOT_ID}`\n𖢵 ɴᴀᴍᴇ : {BOT_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{BOT_USERNAME}")
+        await app.send_message(SUNAME, f"Toga Music Started !")
     except:
-        LOGGER("FallenMusic").error(f"{BOT_NAME} failed to send message at @{SUNAME}, please go & check.")
+        LOGGER("TOGA").error(f"{BOT_NAME} failed to send message at @{SUNAME}, please go & check.")
 
     try:
-        await app2.send_message(SUNAME, f"✯ ғᴀʟʟᴇɴ ᴍᴜsɪᴄ ᴀss ✯\n\n𖢵 ɪᴅ : `{ASS_ID}`\n𖢵 ɴᴀᴍᴇ : {ASS_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{ASS_USERNAME}")
+        await app2.send_message(SUNAME, f"Toga Assistant Started !")
     except:
-        LOGGER("FallenMusic").error(f"{ASS_NAME} failed to send message at @{SUNAME}, please go & check.")
+        LOGGER("TOGA").error(f"{ASS_NAME} failed to send message at @{SUNAME}, please go & check.")
 
-    LOGGER("FallenMusic").info(f"[•] Bot Started As {BOT_NAME}.")
-    LOGGER("FallenMusic").info(f"[•] Assistant Started As {ASS_NAME}.")
+    LOGGER("TOGA").info(f"[•] Bot Started As {BOT_NAME}.")
+    LOGGER("TOGA").info(f"[•] Assistant Started As {ASS_NAME}.")
 
-    LOGGER("FallenMusic").info("[•] \x53\x74\x61\x72\x74\x69\x6e\x67\x20\x50\x79\x54\x67\x43\x61\x6c\x6c\x73\x20\x43\x6c\x69\x65\x6e\x74\x2e\x2e\x2e")
+    LOGGER("TOGA").info("[•] \x53\x74\x61\x72\x74\x69\x6e\x67\x20\x50\x79\x54\x67\x43\x61\x6c\x6c\x73\x20\x43\x6c\x69\x65\x6e\x74\x2e\x2e\x2e")
     await pytgcalls.start()
     try:
         await pytgcalls.join_group_call(
@@ -60,4 +60,4 @@ loop = asyncio.get_event_loop()
 
 if __name__ == "__main__":
     loop.run_until_complete(fallen_startup())
-    LOGGER("FallenMusic").error("Fallen Music Bot Stopped.")
+    LOGGER("TOGA").error("Toga Music Bot Stopped.")
