090977e9e16dae81d6ab09ca503465fbc96f97b4
KAC CHAN <kacxxchan@gmail.com>
2022-11-28 13:12:23 +0530

    Update eval.py

--- TOGA/Modules/eval.py
+++ TOGA/Modules/eval.py
@@ -39,7 +39,7 @@ async def edit_or_reply(msg: Message, **kwargs):
 )
 async def executor(client, message):
     if len(message.command) < 2:
-        return await edit_or_reply(message, text="**ᴡʜᴀᴛ ʏᴏᴜ ᴡᴀɴɴᴀ ᴇxᴇᴄᴜᴛᴇ ʙᴀʙʏ ?**")
+        return await edit_or_reply(message, text="**ᴡʜᴀᴛ ʏᴏᴜ ᴡᴀɴɴᴀ ᴇxᴇᴄᴜᴛᴇ ?**")
     try:
         cmd = message.text.split(" ", maxsplit=1)[1]
     except IndexError:
