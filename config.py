f38e1470ba2c53b18dab61b748a91625d0826983
mimzx <105223184+mimzx@users.noreply.github.com>
2023-01-18 01:35:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,3 +24,5 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 
 
 FAILED = "https://telegra.ph/file/2fc2b008e321ae4592ee4.jpg"
+
+SESSION = getenv("SESSION", "AQAkGxMR326usVUVHWXl8A_AKlbP0M2Be5gHyTsnjrVVQdqYhIo4cy6vIl7HTNOX4cHFTfglweBoxt93w6W3yRlQjglifZIkwyHJ5gtqMH1gSyi6_SRKCWt9EsqllRB_rcDLlqC7Y72Fr0ttETciRJ6807KfCLRiTWz_GlodsnYsZ33pW8ZAggYn8hOFooZYHXz_hQedGCZS3YmtNx9p3SecKew7HahSLfDAXPY3g7mguEmQi94-mKT6onVgFePbG7vHohCB4OjTS6qT2-LE2i2JXhK4S_MvopuZuyqIIQdcusB83pPy2MU3KMxDhab3C0j7lTuZcxlF_xewpq07jEv3AAAAAVJP7wkA")
