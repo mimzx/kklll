2826e56dd1326c8ee531e19d5626c3670d1c722d
mimzx <105223184+mimzx@users.noreply.github.com>
2023-01-18 01:25:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = getenv("API_HASH", "858d65155253af8632221240c535c314")
 BOT_TOKEN = getenv("BOT_TOKEN", "5654221248:AAEKuIi39nwQNFXFwEr8YTgpC2YTDRNSH1s")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID", "5675937545", "5214808179", "5589385763", "5163444566"))
+OWNER_ID = int(getenv("OWNER_ID", "5675937545"))
 
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/4faa7266294d6040d8084.png")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/2fc2b008e321ae4592ee4.jpg")
@@ -20,7 +20,7 @@ SESSION = getenv("SESSION", "AQAkGxMR326usVUVHWXl8A_AKlbP0M2Be5gHyTsnjrVVQdqYhIo
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TogaSupport")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TogaSupport")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5214808179", "5589385763", "5163444566").split()))
 
 
 FAILED = "https://telegra.ph/file/2fc2b008e321ae4592ee4.jpg"
